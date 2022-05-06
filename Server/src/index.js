98ea9dbaa1f344e260ef1c2726021fc4bfaaa62b
SafvanKhWork <safvankhalifa.y@gmail.com>
2022-05-06 12:17:00 +0530

    make it better

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,5 +1,5 @@
-//require("dotenv").config({ path: "../.env" });
-require("dotenv").config();
+require("dotenv").config({ path: "../.env" });
+//require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
