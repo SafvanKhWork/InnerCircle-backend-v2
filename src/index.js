71413f0f0377b68ebd3157cba248eef3b9c645dd
SafvanKhWork <safvankhalifa.y@gmail.com>
2022-05-10 13:56:23 +0530

    ..

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors());
-app.use("/uploads", express.static("../uploads"));
+app.use("/uploads", express.static("./uploads"));
 app.use(express.json());
 
 app.use(userRouter);
